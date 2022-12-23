3749a21dbf8fce9f44c1402122bcfa489770343d
Sandesh Hyoju <sundesz@gmail.com>
2022-12-23 16:42:29 +0200

    Remove unwanted files

--- docs/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SSE_INTERVAL = exports.API_SERVICE_URL = exports.PORT = void 0;
-const utils_1 = require("./utils");
-exports.PORT = process.env.PORT || 5001;
-exports.API_SERVICE_URL = (0, utils_1.parseString)('http://assignments.reaktor.com/birdnest');
-exports.SSE_INTERVAL = 5 * 1000;
--- docs/middleware/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.unknownEndpoint = void 0;
-const unknownEndpoint = (req, res) => {
-    res.status(404).json({ message: `Unknown endpoint ${req.originalUrl}` });
-};
-exports.unknownEndpoint = unknownEndpoint;
